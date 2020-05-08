75145a43e8fb115513e0b84693d08ea6953b2c7a
meilisa21 <62790692+meilisa21@users.noreply.github.com>
2020-05-08 19:10:16 +0700

    Add files via upload

---
+++ modules/pagination/views/_part/navbar.php
@@ -0,0 +1,33 @@
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="<?php echo base_url('admin/dashboard') ?>" class="nav-link">Home</a>
+        </li>
+    </ul>
+
+    <!-- SEARCH FORM -->
+    
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+        <!-- Messages Dropdown Menu -->
+                    <!-- Message Start -->
+                    
+                    <!-- Message End -->
+          
+                
+                
+                    <!-- Message Start -->
+                    
+                    <!-- Message End -->
+
+        <!-- Notifications Dropdown Menu -->
+        
+        <li class="nav-item">
+            <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#"><i class="fas fa-th-large"></i></a>
+    </ul>
+</nav>
